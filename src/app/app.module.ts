78f373f7af5862e1da80093f4de6dd9f1fa8004b
nguyenkhanhtung <tungnk0711@gmail.com>
2019-06-13 16:36:33 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountdownTimerEventComponent } from './countdown-timer-event/countdown-timer-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountdownTimerEventComponent
   ],
   imports: [
     BrowserModule,
